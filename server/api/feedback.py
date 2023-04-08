b6c7b6adc9563c66c06645c85d24fbc93e5796e8
Priya S <priyasridharandav@gmail.com>
2023-04-08 21:14:33 +0530

    (feat): Added internal ratings system and optimised queries

--- server/api/feedback.py
+++ server/api/feedback.py
@@ -2,7 +2,7 @@ import database
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
-from commons.utils import get_prompt_from_prompt_id, update_user_rating
+from commons.utils import get_prompt_from_prompt_id, update_chatbot_user_rating
 from database_constants import PromptRating
 
 feedback_router = APIRouter(prefix="", tags=["feedback"])
@@ -12,18 +12,12 @@ class FeedbackModel(BaseModel):
     score: PromptRating
 
 
-@feedback_router.post("/feedback", status_code=200)
+@feedback_router.put("/feedback", status_code=200)
 def post_internal_user_feedback(
     inputs: FeedbackModel,
     prompt_id: int,
     db: Session = Depends(database.db_session),
 ):
 
-    prompt = get_prompt_from_prompt_id(prompt_id)
-    if prompt is not None:
-        feedback = update_user_rating(prompt_id, inputs.score)
-    else:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Unable to find the prompt",
-        )
+    feedback = update_chatbot_user_rating(prompt_id, inputs.score)
+    return {"message": "Internal rating updated", "rating": inputs.score}
