5491bc2bbc6d5ecaffb7df585a024c79bb932b0d
Chandrani H <chandrani@nimblebox.ai>
2023-04-08 15:11:51 +0530

    feat: chatbot post api with user authentication

---
+++ server/api/chatbot.py
@@ -0,0 +1,44 @@
+import database
+from typing import Annotated
+from database import ChatBot
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, Query, Header
+from pydantic import BaseModel
+from commons.utils import get_user_from_jwt, verify_user
+
+chatbot_router = APIRouter(prefix="/chatbot", tags=["chatbot"])
+
+class ChatBotModel(BaseModel):
+    name: str
+    created_by: str
+    dag: str
+
+@chatbot_router.post("/", status_code=200)
+def create_chatbot(inputs: ChatBotModel, token: Annotated[str, Header()], db: Session = Depends(database.db_session)):
+    username = get_user_from_jwt(token)
+    verify_user(username)
+    try:
+        chatbot = ChatBot(name=inputs.name, created_by=inputs.created_by, dag=inputs.dag)
+        db.add(chatbot)
+        db.commit()
+        response = {"msg": "success"}
+    except Exception as e:
+        print(e)
+        response = {"msg": "failed"}
+    return response
+
+# @chatbot_router.get("/", status_code=200)
+
+# @chatbot_router.put("/", status_code=200)
+# def update_chatbot(inputs: ChatBotModel, db: Session = Depends(database.db_session)):
+    # user: User = db.query(User).filter((User.username == inputs.username) & (User.password == inputs.old_password)).first()
+    # if user is not None:
+    #     user.password = inputs.new_password
+    #     db.commit()
+    #     response = {"msg": "success"}
+    # else:
+    #     response = {"msg": "failed"}
+    # return response
+
+# @chatbot_router.delete("/", status_code=200)
+# def update_chatbot(inputs: ChatBotModel, db: Session = Depends(database.db_session)):
