218d5881c014a67133a734aed5ce2dd4331d0207
Rohan Pooniwala <rohan.pooniwala@gmail.com>
2023-04-09 23:40:26 +0530

    chore: Reformatting imports and adding redirect

--- server/app.py
+++ server/app.py
@@ -1,23 +1,23 @@
 import os
 from typing import Dict, List
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import Response
-from commons import config as c
-from commons.config import engine
-from commons.utils import add_default_user
-from api.chatbot import chatbot_router
-from api.auth import auth_router
+from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
+from commons import config as c
+from commons.utils import add_default_user
 
 # Routers
-from api.user import user_router
-from api.metrics import metrics_router
+from api.auth import auth_router
+from api.chatbot import chatbot_router
 from api.feedback import feedback_router
 from api.intermediate_steps import intermediate_steps_router
-from api.template import template_router
 from api.langflow import router as langflow_router
+from api.metrics import metrics_router
 from api.prompts import router as prompts_router
+from api.template import template_router
+from api.user import user_router
 
 logger = c.get_logger(__name__)
 
@@ -55,6 +55,9 @@ app.include_router(template_router, prefix=API_URL)
 ####################################################
 
 
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/ui/{rest_of_path:path}")
 async def serve_spa(request: Request, rest_of_path: str):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -65,15 +68,11 @@ if "static" not in os.listdir("./"):
     logger.info("Static folder not found. Creating one...")
     os.mkdir("static")
 
+
+@app.get("/", response_class=RedirectResponse, status_code=302)
+async def redirect_pydantic():
+    return "/ui/login"
+
+
 # add static files
 app.mount("/", StaticFiles(directory="static"), name="assets")
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-
-templates = Jinja2Templates(directory="templates")
-
-
-# TO CHECK IF SERVER IS ON
-@app.get("/test", status_code=200)
-def test(response: Response):
-    return {"msg": "success"}
